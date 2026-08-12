3610e06dd81930f4a26e01f31af9df20a61c29c8
Lucas Souto Silva <lucaslenytony@gmail.com>
2026-08-12 20:32:10 -0300

    Add files via upload

---
+++ src/flamingo/aprendendo/introducao/Comentario.java
@@ -0,0 +1,7 @@
+package flamingo.aprendendo.introducao;
+
+public class Comentario {
+    public static void main(String[] args) {
+        System.out.println("Comentario");
+    }
+}
---
+++ src/flamingo/aprendendo/introducao/TiposPrimitivos.java
@@ -0,0 +1,12 @@
+package flamingo.aprendendo.introducao;
+
+public class TiposPrimitivos {
+
+    public static void main(String[] args) {
+        // byte : -128 a 127
+        // short : -32.768 a 32.767
+        byte idade = 27;
+        int 
+        System.out.println("Minha idade é " + idade);
+    }
+}
